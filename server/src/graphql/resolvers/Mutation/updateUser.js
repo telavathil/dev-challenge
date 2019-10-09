c21e85c3ff3d9e80a4a444782b0d22dfdb94b167
Tobin Elavathil <tobin.elavathil@gmail.com>
2019-10-09 13:23:46 -0400

    refactor updateUser

--- server/src/graphql/resolvers/Mutation/updateUser.js
+++ server/src/graphql/resolvers/Mutation/updateUser.js
@@ -1,4 +1,4 @@
-import { setUser } from '../../../helpers';
+import { setUser, getUser } from '../../../helpers';
 
 export default async function User(root, { user }, { ctx }, info) {
   // todo: 1 this throws a unfriendly (and potentially unsafe) error if a non-existnant user ID is entered.
@@ -6,8 +6,20 @@ export default async function User(root, { user }, { ctx }, info) {
 
   // todo: 2 why is this update overwriting existing user data? Need to fix this so that just data input is
   // updated rather than overwritting all the data.
-
-  await setUser(user);
-
-  return true;
+  try {
+    const isUser = await getUser(user.id);
+    await setUser({
+      ...isUser,
+      name: user.name,
+      email: user.email
+    });
+    return true;
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      console.log('user not found');
+    } else {
+      console.log(err.message);
+    }
+    return false;
+  }
 }
