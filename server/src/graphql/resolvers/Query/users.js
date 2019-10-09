4f80d8907163f1577006751e7d289f3b65d434e4
Tobin Elavathil <tobin.elavathil@gmail.com>
2019-10-09 16:57:23 -0400

    add name filter

--- server/src/graphql/resolvers/Query/users.js
+++ server/src/graphql/resolvers/Query/users.js
@@ -5,7 +5,7 @@ const readDir = util.promisify(fs.readdir);
 
 import { getUser } from '../../../helpers';
 
-export default async function users(root, args, { ctx }, info) {
+export default async function users(root, { name }, { ctx }, info) {
   const files = await readDir('./data/users');
 
   // todo: 3. can we accept a input variable into the graphql query to only show certain users? Maybe allowing
@@ -14,9 +14,11 @@ export default async function users(root, args, { ctx }, info) {
   // todo: 5. getting this list of all users is slow.  Would be really cool if it could return all the users
   //  in a more performant way.  Keeping in mind that the underlaying JSON files may get updated.
 
-  const users = files
-    .filter(filename => filename.includes('.json'))
-    .map(filename => getUser(filename.replace('.json', '')));
-
-  return users;
+  const users = await Promise.all(
+    files
+      .filter(filename => filename.includes('.json'))
+      .map(filename => getUser(filename.replace('.json', '')))
+  );
+  const filterUsers = users.filter(user => user.name === name);
+  return filterUsers.length !== 0 ? filterUsers : users;
 }
