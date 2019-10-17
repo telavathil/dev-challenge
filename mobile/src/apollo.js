e464c8246b69fb532e4103ee6bd106969864c01a
Tobin Elavathil <tobin.elavathil@gmail.com>
2019-10-17 11:35:07 -0400

    remove hardcoded ip address

--- mobile/src/apollo.js
+++ mobile/src/apollo.js
@@ -3,9 +3,7 @@ import { AsyncStorage } from 'react-native';
 import { persistCache } from 'apollo-cache-persist';
 
 // note: this may not work on android, you will probably have to put your IP address here
-// todo: fix
-const hostAddress = '192.168.86.60' || 'localhost';
-const address = `http://${hostAddress}:5000/graphql`;
+const address = `http://localhost:5000/graphql`;
 
 const cache = new InMemoryCache();
 
